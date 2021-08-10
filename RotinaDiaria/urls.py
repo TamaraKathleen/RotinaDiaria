a2f6235a92b9269f0697bfb6c991733ea348575c
Tamara Kathleen <tamara.rocha@ebc.com.br>
2021-08-10 10:37:03 -0300

    Criar arquivo static

--- RotinaDiaria/urls.py
+++ RotinaDiaria/urls.py
@@ -2,9 +2,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('cadastro.urls')),
-    path('admin/', admin.site.urls),
-    
-]
+    path('admin/', admin.site.urls),   
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
